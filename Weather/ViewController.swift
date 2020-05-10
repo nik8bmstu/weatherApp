4056964cdba0f18c4e6a47a28766228bd65bc47c
Nik <nik-nfsu@ya.ru>
2020-05-11 01:28:46 +0300

    Added icons Added gradient background

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dayLabel: UILabel!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var backgroundView: UIView!
+    
+    let gradientLayer = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello, World!")
+        backgroundView.layer.addSublayer(gradientLayer)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setDayBackground()
     }
 
+    func setDayBackground(){
+        let topColor = UIColor(red: 50.0/255.0, green: 220.0/255.0, blue: 255.0/255.0, alpha: 1.0).cgColor
+        let botColor = UIColor(red: 20.0/255.0, green: 120.0/255.0, blue: 180.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, botColor]
+    }
+    
+    func setNightBackground(){
+        let topColor = UIColor(red: 20.0/255.0, green: 150.0/255.0, blue: 150.0/255.0, alpha: 1.0).cgColor
+        let botColor = UIColor(red: 0.0/255.0, green: 80.0/255.0, blue: 80.0/255.0, alpha: 1.0).cgColor
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [topColor, botColor]
+    }
 
 }
 
